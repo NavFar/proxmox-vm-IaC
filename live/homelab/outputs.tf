21050c5202bd490aff9cbcac5f5035f99b2ffdd2
navidfarahmand <navidfarahmand75@gmail.com>
2026-06-24 19:56:30 +0330

    feat: show ha rules in output"

--- live/homelab/outputs.tf
+++ live/homelab/outputs.tf
@@ -2,3 +2,7 @@ output "vms" {
   description = "VM details:"
   value       = module.vms.vms
 }
+output "resource_affinity"{
+  description = "resource affinity details"
+  value       = module.vms.ha_resource_rules
+}
